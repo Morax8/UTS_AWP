3f7b39e8d97b2070bb7b29ac4bd1871c96006f82
Aditya Zianur <adityazianurrahman@gmail.com>
2025-12-20 21:39:51 +0700

    small fix on typo

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 // 1. Import motion
 import { motion } from "framer-motion";
 import zian from "/images/zian.jpg";
-import khairi from "/images/khairi/jpeg";
+import khairi from "/images/khairi.jpeg";
 
 // --- Komponen Ikon SVG (Tidak berubah) ---
 const CheckIcon = () => (
